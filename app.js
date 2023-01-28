d1db478976e60f3abca0c797766b8fd0eb45c271
benejorg <84802868+benejorg@users.noreply.github.com>
2023-01-28 19:55:53 +0100

    Added functionality to dropdown select

--- app.js
+++ app.js
@@ -20,7 +20,29 @@ function londonTime() {
   londonTimeElement.innerHTML = moment().tz("Europe/London").format("HH:mm:ss");
 }
 
+// Update every second
 newYorkTime();
 londonTime();
 setInterval(newYorkTime, 1000);
 setInterval(londonTime, 1000);
+
+// Dropdown box
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone).format("dddd Do MMMM YYYY");
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+  <div>
+    <h2>${cityName}</h2>
+    <div class="date">${moment()
+      .tz(cityTimeZone)
+      .format("dddd Do MMMM YYYY")}</div>
+  </div>
+  <div class="time">${moment().tz(cityTimeZone).format("HH:mm:ss")}</div>
+</div>`;
+}
+
+let citiesSelectElement = document.querySelector("#citySelector");
+
+citiesSelectElement.addEventListener("change", updateCity);
